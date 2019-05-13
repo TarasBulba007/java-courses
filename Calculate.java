b97d946342a794744af2f20930f943ff8cbb6069
TarasBulba007 <mr.arra@yandex.ru>
2019-05-13 07:28:56 +0300

    user input

--- Calculate.java
+++ Calculate.java
@@ -2,5 +2,13 @@ public class Calculate {
 
 	public static void main(String[] arg) {
 		System.out.println("Calculate");
+		double first = Double.valueOf(arg[0]);
+		double second= Double.valueOf(arg[1]);
+		double summ = first + second;
+		double diff = second - first;
+		double dev = second / first;
+		double stepen = Math.pow(first, second);
+		System.out.println("Summ = " + summ +  '\n' + "Diff = " + diff + 
+		'\n' + "dev = " + dev + '\n' + "stepen = " + stepen);
 	}
 }
